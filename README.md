896ed8ba638221fed9de292964a663a1c5914cfb
ivanjermakov <ivanjermakov1@gmail.com>
2025-12-02 18:39:48 +0100

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Scoundrel
+
+Online version of a single player rogue-like card game by Zach Gage and Kurt Bieg
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+/* @refresh reload */
+import { Component, For, Match, Show, Switch, createResource, createSignal, onMount } from 'solid-js'
+import { render } from 'solid-js/web'
+import './index.css'
+
+const Main: Component = () => {
+    return <span>scoundrel</span>
+}
+
+render(() => <Main />, document.getElementById('root')!)
